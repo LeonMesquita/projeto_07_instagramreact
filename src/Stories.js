663e566b43991f1109eb3b0534c0ea57f0aae721
LeonMesquita <leonmesquita16@gmail.com>
2022-05-05 23:55:46 -0300

    Componentizar os stories usando o props

--- src/Stories.js
+++ src/Stories.js
@@ -1,92 +1,29 @@
 export default function Stories(){
     return (
         <div class="stories-box">
-            <div class="storie">
-                <img class="storie-circle" src="images/stories_background.jpg" />
-                <a href="">
-                    <img class="storie-image" src="images/profiles/9gag 1.png" alt="" />
-                </a>
-
-            <div class="storie-text">9gag</div>
-            </div>
-
-    
-            <div class="storie">
-                <img class="storie-circle" src="images/stories_background.jpg" />
-                <a href="">
-                    <img class="storie-image" src="images/profiles/meowed 1.png" alt="" />
-                </a>
-
-            <div class="storie-text">meowed</div>
-            </div>
-
-    
-            <div class="storie">
-                <img class="storie-circle" src="images/stories_background.jpg" />
-                <a href="">
-                    <img class="storie-image" src="images/profiles/barked 1.png" alt="" />
-                </a>
-
-            <div class="storie-text">barked</div>
-            </div>
-
-    
-            <div class="storie">
-                <img class="storie-circle" src="images/stories_background.jpg" />
-                <a href="">
-                    <img class="storie-image" src="images/profiles/nathanwpylestrangeplanet 1.png" alt="" />
-                </a>
-
-            <div class="storie-text">nathan</div>
-            </div>
-
-    
-            <div class="storie">
-                <img class="storie-circle" src="images/stories_background.jpg" />
-                <a href="">
-                    <img class="storie-image" src="images/profiles/wawawiwacomicsa 1.png" alt="" />
-                </a>
-
-            <div class="storie-text">wawa</div>
-            </div>
-
-    
-            <div class="storie">
-                <img class="storie-circle" src="images/stories_background.jpg" />
-                <a href="">
-                    <img class="storie-image" src="images/profiles/respondeai 1.png" alt="" />
-                </a>
-
-            <div class="storie-text">respondeai</div>
-            </div>
-
-    
-            <div class="storie">
-                <img class="storie-circle" src="images/stories_background.jpg" />
-                <a href="">
-                    <img class="storie-image" src="images/profiles/filomoderna 1.png" alt="" />
-                </a>
-
-            <div class="storie-text">filo</div>
-            </div>
-
-    
-            <div class="storie">
-                <img class="storie-circle" src="images/stories_background.jpg" />
-                <a href="">
-                    <img class="storie-image" src="images/profiles/memeriagourmet 1.png" alt="" />
-                </a>
-
-            <div class="storie-text">meme</div>
-            </div>
-
-
-    
-
-            <ion-icon class="stories-button" name="chevron-forward-outline"></ion-icon>
-            
-            
+            <Storie src="images/profiles/9gag 1.png" text="9gag"/>
+            <Storie src="images/profiles/meowed 1.png" text="meowed"/>
+            <Storie src="images/profiles/barked 1.png" text="barked"/>
+            <Storie src="images/profiles/nathanwpylestrangeplanet 1.png" text="nathan"/>
+            <Storie src="images/profiles/wawawiwacomicsa 1.png" text="wawa"/>
+            <Storie src="images/profiles/respondeai 1.png" text="respondeai"/>
+            <Storie src="images/profiles/filomoderna 1.png" text="filo"/>
+            <Storie src="images/profiles/memeriagourmet 1.png" text="meme"/>
+            <ion-icon class="stories-button" name="chevron-forward-outline"></ion-icon>    
         </div>
 
     )
+}
+
+function Storie(props){
+    return (
+        <div class="storie">
+            <img class="storie-circle" src="images/stories_background.jpg" />
+            <a href="">
+                <img class="storie-image" src={props.src} alt="" />
+            </a>
+
+            <div class="storie-text">{props.text}</div>
+        </div>
+    );
 }
