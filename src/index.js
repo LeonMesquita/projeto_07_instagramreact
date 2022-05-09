439a09c8c1e9b9a25fea9616785036f2f83c4207
LeonMesquita <leonmesquita16@gmail.com>
2022-05-09 14:11:09 -0300

    ajustes finais

--- src/index.js
+++ src/index.js
@@ -2,39 +2,9 @@ import ReactDOM from 'react-dom';
 import Posts from './Posts';
 import SideBar from './SideBar';
 import Stories from './Stories';
+import TopBar from './TopBar';
 
-function TopBar(){
-    return (
-       // <!--barra superior-->
-        <div class="bar-father">
-          <div class="top-bar">
-              <div class="div-logo">
-                  <a href="/"> <div><img class="insta-icon" src="images/icons/logo-instagram.svg"  alt=""/></div></a>
-                  <div class="divide"></div>
-                  <a href="/"> <div><img class="insta-logo" src="images/Instagram.png"  alt=""/></div></a>
-              </div>
-      
-              <div>
-              
-                  <input  class="divBusca" type="text" id="txtBusca" placeholder="Pesquisar"/>
-              </div>
-              <div class="div-logo">
-                  <a href="/"><ion-icon name="paper-plane-outline"   class="top-icons"></ion-icon></a>
-                  <a href="/"><ion-icon name="compass-outline"   class="top-icons"></ion-icon></a>
-                <a href="/"><ion-icon name="heart-outline"  class="top-icons"></ion-icon></a>
-                 <a href="/"><ion-icon name="person-outline"  class="top-icons"></ion-icon></a>
-              </div>
-              
-          </div>
-      
-          <div class="mobile-bar">
-              <a href="/"> <div><img class="insta-icon" src="images/icons/logo-instagram.svg"  alt=""/></div></a>
-              <a href="/"> <div><img class="insta-logo" src="images/Instagram.png"  alt=""/></div></a>
-              <a href="/"><ion-icon name="paper-plane-outline"   class="top-icons"></ion-icon></a>
-          </div>
-          </div>
-    )
-}
+
 
 function AppBody(){
     return (
